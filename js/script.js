cef25429d9313e3e86ae92593c87c03922543e36
Ginger-Stone <doreenjeptoo@gmail.com>
2020-06-04 06:46:09 +0300

    Worked on filters and their responsivity

--- js/script.js
+++ js/script.js
@@ -38,6 +38,16 @@ function showNav(notSeen, width) {
   // });
 }
 
+function showFilter() {
+  let filter = document.querySelector("aside");
+  filter.classList.remove("visibility");
+}
+
+function closeFilter() {
+  let filter = document.querySelector("aside");
+  filter.classList.add("visibility");
+}
+
 function backItModal() {
   let backIt = document.getElementById("backit-modal");
   let page = document.getElementsByClassName("project-details")[0];
@@ -53,3 +63,153 @@ function cancel() {
   backIt.style = "visibility:hidden; display:none;";
   page.style = "visibility:visible; display:'';";
 }
+
+// Filters with searches
+document.getElementById("search").addEventListener("keyup", filterList);
+
+function filterList() {
+  let filter = document.getElementById("search");
+  // let wordlist = document.getElementById("wordlist");
+  let category = document.querySelectorAll(".category-filter");
+  console.log(category);
+  let i = 0;
+  while (i < category.length) {
+    let value = category[i].innerText.toUpperCase();
+    if (value.indexOf(filter.value.toUpperCase()) === -1) {
+      category[i].parentElement.style =
+        "visibility: hidden; display:none; transition:all 500ms ease-in";
+    } else {
+      category[i].parentElement.style =
+        "visibility: visible; display:''; transition:all 500ms ease-in";
+    }
+
+    i++;
+  }
+}
+
+document
+  .getElementById("location-search")
+  .addEventListener("keyup", filterListCity);
+
+function filterListCity() {
+  let filter = document.getElementById("location-search");
+  // let wordlist = document.getElementById("wordlist");
+  let category = document.querySelectorAll(".city-filter");
+  // console.log(category);
+  let i = 0;
+  while (i < category.length) {
+    let value = category[i].innerHTML.toUpperCase();
+    if (value.indexOf(filter.value.toUpperCase()) === -1) {
+      category[i].parentElement.style =
+        "visibility: hidden; display:none; transition:all 500ms ease-in";
+    } else {
+      category[i].parentElement.style =
+        "visibility: visible; display:''; transition:all 500ms ease-in";
+    }
+
+    i++;
+  }
+}
+
+// Filters with checkboxes
+document
+  .getElementsByClassName("item-content")[0]
+  .addEventListener("click", filterCheck);
+function filterCheck() {
+  let data = document.querySelectorAll(".div-item h4.category-filter");
+  let checkboxItems = document.querySelectorAll(".item");
+  let i = 0;
+  let status = false;
+  while (i < data.length) {
+    let value = data[i].innerHTML;
+    console.log(value);
+    let present = false;
+    let j = 0;
+    let val;
+    while (j < checkboxItems.length) {
+      if (checkboxItems[j].checked === true) {
+        status = true;
+        val = checkboxItems[j].value;
+
+        //   console.log("value vs val" + value, val, val == value);
+        if (value == val) {
+          present = true;
+          break;
+        }
+      }
+      j++;
+    }
+    if (present == true) {
+      // console.log(data[i]);
+      data[i].parentElement.style = "visibility:visible; display:''";
+    } else {
+      data[i].parentElement.style = "visibility:hidden; display:none";
+    }
+    i++;
+  }
+
+  if (status == false) {
+    let j = 0;
+    while (j < data.length) {
+      let val = data[j].value;
+      // console.log("value vs val" + value, val);
+
+      data[j].parentElement.style = "visibility:visible; display:''";
+
+      j++;
+    }
+  }
+  console.log(status);
+  i++;
+}
+
+// document
+//   .getElementsByClassName("item-content")[2]
+//   .addEventListener("click", filterCheckTiming);
+// function filterCheckTiming() {
+//   let data = document.querySelectorAll(".div-item h4.time-filter");
+//   let checkboxItems = document.querySelectorAll(".item");
+//   let i = 0;
+//   let status = false;
+//   while (i < data.length) {
+//     let value = data[i].innerHTML;
+//     console.log(value);
+//     let present = false;
+//     let j = 0;
+//     let val;
+//     while (j < checkboxItems.length) {
+//       if (checkboxItems[j].checked === true) {
+//         status = true;
+//         val = checkboxItems[j].value;
+
+//         //   console.log("value vs val" + value, val, val == value);
+//         if (value == val) {
+//           present = true;
+//           break;
+//         }
+//       }
+//       j++;
+//     }
+//     if (present == true) {
+//       // console.log(data[i]);
+//       data[i].parentElement.style = "visibility:visible; display:''";
+//     } else {
+//       data[i].parentElement.style = "visibility:hidden; display:none";
+//     }
+//     i++;
+//   }
+
+//   if (status == false) {
+//     let j = 0;
+//     while (j < data.length) {
+//       let val = data[j].value;
+//       // console.log("value vs val" + value, val);
+
+//       data[j].parentElement.style = "visibility:visible; display:''";
+
+//       j++;
+//     }
+//   }
+//   console.log(status);
+//   i++;
+// }
